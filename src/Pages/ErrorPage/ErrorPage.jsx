da3be7172e2fc27c876311d9db02f9044e171737
Jihad Hasan <mdjihat0021@gmail.com>
2023-12-22 12:50:18 +0600

    finished error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -9,13 +9,13 @@ const ErrorPage = () => {
                 <title>ErrorPage || Fitness Tracker</title>
             </Helmet>
             <div className="flex flex-col justify-center items-center">
-                <img className="w-300px h-150px md:w-[500px] md:h-[300px]" src="https://i.ibb.co/1vjxQW2/3613901.png"  />
-            <h1 className="md:text-4xl text-2xl text-center">Not Found</h1>
+                <img className="w-300px h-150px md:w-[500px] md:h-[300px]" src="https://i.ibb.co/1vjxQW2/3613901.png"  data-aos="fade-up"/>
+            <h1 className="md:text-4xl text-2xl text-center" data-aos="fade-right">Not Found</h1>
             </div>
-                <p className="md:text-2xl text-xl py-4 text-center">The Page You Are Looking For Is Not Avaliable</p>
+                <p className="md:text-2xl text-xl py-4 text-center" data-aos="fade-left">The Page You Are Looking For Is Not Avaliable</p>
             <div className="flex text-center  justify-center">
                 <Link to={'/'}>
-                <button className="btn btn-accent">Go Back Home</button>
+                <button className="btn btn-accent" data-aos="fade-down">Go Back Home</button>
                 </Link>
             </div>
         </div>
